d10b3d6d4bda176d6e73f3868d9aa713fe40e214
ttdjohns <32712899+ttdjohns@users.noreply.github.com>
2019-04-11 15:36:12 -0600

    Completed output file creation in Assignment4v2withABO.cpp

--- Assignment4v2withABO.cpp
+++ Assignment4v2withABO.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <iostream>
 #include <endian.h>
+#include <math.h>
 
 
 using namespace std;
@@ -32,15 +33,17 @@ struct chunk_t
 	unsigned long size;  //Chunk data bytes
 };
 
-float *input_signal;
+double *input_signal;
 unsigned long input_size = 0;
-float *ir_signal;
+double *ir_signal;
 unsigned long ir_size = 0;
 struct header_file headers[3];
 unsigned long size[3];
-float ** signals;
-float ** fftSignals;
+double ** signals;
+double ** fftSignals;
 int nextInd = 0;
+
+#define PI 3.14159265358979323846264338327950288
 
 int readFile(char *name)
 {
@@ -73,7 +76,7 @@ int readFile(char *name)
 	//memset(value, 0, sizeof(short int) * samples_count);
 	printf("value initialized\n");
 
-	signals[nextInd] = new float[count];
+	signals[nextInd] = new double[count];
 	//memset(value, 0, sizeof(float) * samples_count);
 
 	printf("signal initialized\n");
@@ -84,7 +87,8 @@ int readFile(char *name)
 		fread(&value[i], 2, 1, file);
 		signals[nextInd][i] = value[i] / 32768.0;
 	}
-	printf("signals[200] = %f\n", signals[nextInd][200]);
+	//printf("signals[200] = %f\n", signals[nextInd][200]);
+
 
 	size[nextInd] = count;
 
@@ -117,6 +121,7 @@ void fft(double* data, unsigned long nn)
 	// reverse-binary reindexing
 	n = nn << 1;
 	j = 1;
+	printf("entering first for in fft\n");
 	for (i = 1; i < n; i += 2) {
 		if (j > i) {
 			swap(data[j - 1], data[i - 1]);
@@ -129,10 +134,11 @@ void fft(double* data, unsigned long nn)
 		}
 		j += m;
 	};
-
+	printf("finished forst for in fft\n");
 	// here begins the Danielson-Lanczos section
 	mmax = 2;
 	while (n > mmax) {
+		printf("in fft while with n = %lu and mmax = %lu\n", n, mmax);
 		istep = mmax << 1;
 		theta = -(2 * M_PI / mmax);
 		wtemp = sin(0.5*theta);
@@ -175,10 +181,10 @@ ifft(double **v, unsigned long n, double **tmp)
 		ifft(ve, n << 1, v);		/* FFT on even-indexed elements of v[] */
 		ifft(vo, n << 1, v);		/* FFT on odd-indexed elements of v[] */
 		for (m = 0; m < n / 2; m++) {
-			w[0] = cos(2 * PI*m / (double)n);
-			w[1] = sin(2 * PI*m / (double)n);
-			z[0] = w[0]*vo[m][0] - w[1]*vo[m][1];	/* Re(w*vo[m]) */
-			z[1] = w[0]*vo[m][1] + w[1]*vo[m][0];	/* Im(w*vo[m]) */
+			w[0] = cos(2 * m * PI / (double)n);
+			w[1] = sin(2 * m * PI / (double)n);
+			z[0] = w[0] * vo[m][0] - w[1] * vo[m][1];	/* Re(w*vo[m]) */
+			z[1] = w[0] * vo[m][1] + w[1] * vo[m][0];	/* Im(w*vo[m]) */
 			v[m][0] = ve[m][0] + z[0];
 			v[m][1] = ve[m][1] + z[1];
 			v[m + n / 2][0] = ve[m][0] - z[0];
@@ -188,13 +194,43 @@ ifft(double **v, unsigned long n, double **tmp)
 	return;
 }
 
+void writeHeader(FILE* file) {
+	unsigned short int *siBuff = new unsigned short int[4];
+	unsigned int *iBuff = new unsigned int[4];
+
+	fwrite(headers[0].chunk_id, 4, 1, file);
+	iBuff[0] = htole32(headers[0].chunk_size);
+	fwrite(iBuff, 4, 1, file);
+	fwrite(headers[0].format, 4, 1, file);
+	fwrite(headers[0].subchunk1_id, 4, 1, file);
+	iBuff[0] = htole32(headers[0].subchunk1_size);
+	fwrite(iBuff, 4, 1, file);
+	siBuff[0] = htole16(headers[0].audio_format);
+	fwrite(siBuff, 2, 1, file);
+	siBuff[0] = htole16(headers[0].num_channels);
+	fwrite(siBuff, 2, 1, file);
+	iBuff[0] = htole32(headers[0].sample_rate);
+	fwrite(iBuff, 4, 1, file);
+	iBuff[0] = htole32(headers[0].byte_rate);
+	fwrite(iBuff, 4, 1, file);
+	siBuff[0] = htole16(headers[0].block_align);
+	fwrite(siBuff, 2, 1, file);
+	siBuff[0] = htole16(headers[0].bits_per_sample);
+	fwrite(siBuff, 2, 1, file);
+	fwrite(headers[0].subchunk2_id, 4, 1, file);
+	iBuff[0] = htole32(headers[0].subchunk2_size);
+	fwrite(iBuff, 4, 1, file);
+
+	return;
+}
+
 int main(int argc, char ** argv)
 {
 	if (argc != 4) {
 		printf("Usage: ./Assignment4 [input file] [IR file] [output file]\n");
 		return 0;
 	}
-	signals = new float*[3];
+	signals = new double*[3];
 
 	input_size = readFile(argv[1]);
 	//printf("input_size is %lu\n", size[0]);
@@ -202,7 +238,7 @@ int main(int argc, char ** argv)
 	if (!useImpulse) {
 		ir_size = readFile(argv[2]);
 	} else {
-		signals[1] = new float[1];
+		signals[1] = new double[1];
 		signals[1][0] = 1.0;
 		size[1] = 1;//*/
 	}
@@ -216,10 +252,10 @@ int main(int argc, char ** argv)
 	}
 
 
-	fftSignals = new float*[2];
+	fftSignals = new double*[2];
 	//convolve(signals[0], size[0], signals[1], size[1], signals[2], size[2]);
 	for (int i = 0; i < 2; i++) {
-		fftSignals[i] = new float[nn];
+		fftSignals[i] = new double[nn];
 	}
 
 	// put the real part in every second index
@@ -233,19 +269,19 @@ int main(int argc, char ** argv)
 			}
 		}
 	}
-
+	printf("applying fft\n");
 	nn = nn << 1;
 	fft(fftSignals[0], nn);
 	fft(fftSignals[1], nn);
-
+	printf("fft completed \n");
 	nn = nn >> 1;
 	for (unsigned long i = 0; i < nn; i++) {
 		fftSignals[0][i] += fftSignals[1][i];
 	}
 
 	nn = nn << 1;
-	double ** v = new double[nn];
-	double ** tmp = new double[nn];
+	double ** v = new double*[nn];
+	double ** tmp = new double*[nn];
 	for (int i = 0; i < nn; i++) {
 		v[i] = new double[2];
 		tmp[i] = new double[2];
@@ -255,9 +291,10 @@ int main(int argc, char ** argv)
 		v[i][1] = fftSignals[0][(2 * i) + 1];
 	}
 
+	printf("applying ifft \n");
 	ifft(v, nn, tmp);
 
-	printf("finished the convolution\n");
+	printf("finished the ifft\n");
 	short int *buffer = new short int[sizeof(struct header_file) / 2 + size[0]];
 	printf("initialized the ouput buffer\n");
 
@@ -265,18 +302,20 @@ int main(int argc, char ** argv)
 	//fread(&buffer, sizeof(struct header_file), 1, file);
 	//fclose(file);
 	printf("changing the size of the header\n");
-	headers[0].subchunk2_size = htole32(size[0]);
-	memcpy(buffer, &headers[0], sizeof(struct header_file));
+	headers[0].subchunk2_size = size[0];
+	//memcpy(buffer, &headers[0], sizeof(struct header_file));
+
 
 	printf("converting the signal back into short ints\n");
 	for (unsigned long i = 0; i < size[0]; i++) {
-		buffer[sizeof(struct header_file) + i] = (short int)(v[i][0] * 32768);
+		buffer[i] = htole16((short int)(v[i][0] * 32768));
 	}
 	//memcpy(buffer + 40, &size[2], 4);
 	
 	FILE *file;
 	file = fopen(argv[3], "w");
-	fwrite(&buffer, sizeof(struct header_file) + size[0], 1, file);
+	writeHeader(file);
+	fwrite(&buffer, 2, input_size, file);
 	fclose(file);
 
 	printf("finished\n");
